7d924118ebe9f1db19b1d0ed1b6ace4c937303fe
Mahesh Bhojane <maheshbhojane200@gmail.com>
2025-08-07 23:42:15 +0530

    update code

---
+++ Chapter-3/String-functions.py
@@ -0,0 +1,29 @@
+#  Length
+
+name = "Hello"
+
+print(len(name))
+
+
+#  endsWith("llo")
+
+print(name.endswith("llo"))
+
+
+#  strtWith("Hel")
+
+print(name.startswith("Hel"))
+
+
+# Capitalize
+
+print(name.capitalize())
+
+#  Find
+
+print(name.find("Hello"))
+
+
+# replace
+
+print(name.replace("Hello", "Hey"))
---
+++ Chapter-3/String.py
@@ -0,0 +1,24 @@
+# String is a datatypes in python
+# String are immutable 
+
+
+
+# There are three types to create a strings
+# 'ABC'
+# "ABC"
+# """ ABC """
+
+
+name = "Harry"
+
+nameShort = name[0:3] # Start from index 0 all the way till 3 excluding 3
+nameShort = name[-4:-1] 
+nameShort = name[:-1] 
+
+jsut = 'hfyrueo'
+
+print(jsut[1:4:2])
+
+
+print(nameShort)
+ 
