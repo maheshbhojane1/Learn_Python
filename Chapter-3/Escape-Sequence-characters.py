ae81bcaf9e1777bbe25940b57eb38b30570f3d15
Mahesh Bhojane <maheshbhojane200@gmail.com>
2025-08-07 23:58:04 +0530

    new file

---
+++ Chapter-3/Escape-Sequence-characters.py
@@ -0,0 +1,13 @@
+# Escape Sequence Characters
+# Sequence of Characters after backslash "\"
+# Escape Sequence Characters comprEscape Sequence Charactersse of more that one characters but represent one character when used within the strring
+
+a = "Hello my name is Mahesh\ni am a freelancer"  # for new line
+b = "Hello my name is Mahesh\t am a freelancer" # for tab
+c = "Hello my name is Mahesh  am a \'freelancer\'" # for singlequote
+d = "Hello my name is Mahesh\\ am a freelancer" # for backslash
+
+print(a)
+print(b)
+print(c)
+print(d)
