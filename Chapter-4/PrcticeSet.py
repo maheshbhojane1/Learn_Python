b1aefafd53b903b4afc940b65cdf06ec65a46b7c
Mahesh Bhojane <maheshbhojane200@gmail.com>
2025-08-12 00:18:44 +0530

    little bit change

--- Chapter-4/PrcticeSet.py
+++ Chapter-4/PrcticeSet.py
@@ -1,43 +1,43 @@
-# fruits = []
+fruits = []
 
-# f1 = int(input("Enter fruit name: "))
-# fruits.append(f1)
-# f2 = int(input("Enter fruit name: "))
-# fruits.append(f2)
-# f3 = int(input("Enter fruit name: "))
-# fruits.append(f3)
-# f4 = int(input("Enter fruit name: "))
-# fruits.append(f4)
-# f5 = int(input("Enter fruit name: "))
-# fruits.append(f5)
-# f6 = int(input("Enter fruit name: "))
-# fruits.append(f6)
-# f7 = int(input("Enter fruit name: "))
-# fruits.append(f7)
+f1 = int(input("Enter fruit name: "))
+fruits.append(f1)
+f2 = int(input("Enter fruit name: "))
+fruits.append(f2)
+f3 = int(input("Enter fruit name: "))
+fruits.append(f3)
+f4 = int(input("Enter fruit name: "))
+fruits.append(f4)
+f5 = int(input("Enter fruit name: "))
+fruits.append(f5)
+f6 = int(input("Enter fruit name: "))
+fruits.append(f6)
+f7 = int(input("Enter fruit name: "))
+fruits.append(f7)
 
-# # print(fruits)
-
-
+print(fruits)
 
 
 
 
-# marks = [] # we convert string into the int 
 
-# m1 = int(input("Enter Your Marks: "))
-# marks.append(m1)
-# m2 = int(input("Enter Your Marks: "))
-# marks.append(m2)
-# m3 = int(input("Enter Your Marks: "))
-# marks.append(m3)
-# m4 = int(input("Enter Your Marks: "))
-# marks.append(m4)
-# m5 = int(input("Enter Your Marks: "))
-# marks.append(m5)
 
-# marks.sort()
+marks = [] # we convert string into the int 
 
-# print(marks)
+m1 = int(input("Enter Your Marks: "))
+marks.append(m1)
+m2 = int(input("Enter Your Marks: "))
+marks.append(m2)
+m3 = int(input("Enter Your Marks: "))
+marks.append(m3)
+m4 = int(input("Enter Your Marks: "))
+marks.append(m4)
+m5 = int(input("Enter Your Marks: "))
+marks.append(m5)
+
+marks.sort()
+
+print(marks)
 
 
 
